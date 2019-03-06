95f9be5114b115f7f6fa92b4068e3550a49eb2b3
Phil McNamara <phil.j.mcnamara@gmail.com>
2019-03-06 13:57:42 -0800

    very basic drawing functional

--- motif-mark.py
+++ motif-mark.py
@@ -1,6 +1,6 @@
 #! /home/phil/anaconda3/bin/python
 
-# import cairo
+import cairo
 import re
 import argparse
 # import math
@@ -84,7 +84,7 @@ def find_motif_indices(motif_list, sequence_list):
                 motif_matches.append([motif, match_indices])
         if any_matches:
             all_matches.append([sequence_ID, motif_matches])
-    print(all_matches)
+    return all_matches
 
 
 def motif_to_regex(motif):
@@ -114,15 +114,53 @@ def motif_to_regex(motif):
     return pattern
 
 
-def draw_figures():
-    pass
+def draw_figures(matches):
+    width = 1500
+    # Length of matches = number of sequences, each sequence gets an independent figure
+    height = 1000 * len(matches)
+    surface = cairo.SVGSurface("figure.svg", width, height)
+    ctx = cairo.Context(surface)
+
+    # Draw sequence
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.set_line_width(10)
+    ctx.move_to(100, 800)
+    ctx.line_to(1400, 800)
+    ctx.stroke()
+
+    # Draw exon
+    ctx.rectangle(600, 750, 300, 100) # x,y of top left corner, width, height
+    ctx.fill()
+    
+    # Sequence ID Title
+    ctx.move_to(100, 100)
+    ctx.set_font_size(36)
+    ctx.show_text("Sequence 1")
+
+    # Legend
+    ctx.move_to(1200, 100)
+    ctx.show_text("Legend")
+
+    # Divider to separate next figure
+    ctx.move_to(0, 1000)
+    ctx.set_source_rgb(0.5, 0.5, 0.5)
+    ctx.line_to(1500, 1000)
+    ctx.stroke()
+
 
 
 def main():
     args = get_arguments()
     motif_list = ["GATAC", "MATAM"]
     sequence_list = parse_input_sequence(args.sequence_input)
-    find_motif_indices(motif_list, sequence_list)
+    matches = find_motif_indices(motif_list, sequence_list)
+    # print(matches[0]) # First sequence and matches
+    # print(matches[0][0]) # First sequence ID
+    # print(matches[0][1]) # First sequence list of matching motifs
+    # print(matches[0][1][0]) # First sequence first motif, ID and indices
+    # print(matches[0][1][0][0])  # First sequence first motif ID
+    # print(matches[0][1][0][1])  # first sequence first motif list of indices
+    draw_figures(matches)
 
 
 if __name__ == "__main__":
