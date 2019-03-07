98911378b35d234b711bec863023dcb5e9a94129
Phil McNamara <phil.j.mcnamara@gmail.com>
2019-03-06 16:58:39 -0800

    documentation and scale bar argparse

---
+++ README.md
@@ -0,0 +1,11 @@
+# Motif-mark
+
+motif-mark.py creates graphics to visualize motifs in a given DNA or RNA sequence. It supports up to 10 simultaneous motifs and any number of input sequences from a FASTA file. Output is a SVG file of dimensions 1500 by 1000 * *n* pixels where *n* is the number of input sequences. Motifs are drawn with contrasting colors, and an adjustable scale bar allows for visual comparison.
+
+## Arguments
+
+| -m     | --motifs_input         | input file with one motif per line, max 10        |
+| ------ | ---------------------- | ------------------------------------------------- |
+| **-s** | **--sequence_input**   | **FASTA input file with sequences**               |
+| **-o** | **--output**           | **Output file name (omit extension, always SVG)** |
+| **-l** | **--scale_bar_length** | **Length of the scale bar in bp (base pairs)**    |
--- motif-mark.py
+++ motif-mark.py
@@ -3,7 +3,6 @@
 import cairo
 import re
 import argparse
-# import math
 
 
 def get_arguments():
@@ -19,8 +18,20 @@ def get_arguments():
     # FASTA file with sequences
     parser.add_argument(
         "-s", "--sequence_input",
-        help="FASTA input file with sequences, max 10",
+        help="FASTA input file with sequences",
         required=True, type=str
+    )
+    # Output figure name
+    parser.add_argument(
+        "-o", "--output",
+        help="Output file name (omit extension)",
+        required=False, default="Figure"
+    )
+    # Scale bar
+    parser.add_argument(
+        "-l", "--scale_bar_length",
+        help="length of the scale bar in bp (base pairs)",
+        required=False, default=100, type=int
     )
     return parser.parse_args()
 
@@ -114,25 +125,25 @@ def motif_to_regex(motif):
     return pattern
 
 
-def draw_figures(matches):
+def draw_figures(matches, output, scale_bar_length):
     width = 1500
     # 1000 pixels per subfigure, 1 per sequence
     height = 1000 * len(matches)
-    surface = cairo.SVGSurface("Figure_1.svg", width, height)
+    surface = cairo.SVGSurface(output + ".svg", width, height)
     ctx = cairo.Context(surface)
 
     # RGB colors for marking the motifs
     colors = [
-        [0.2, 0.7, 0.2],  # green
+        [0.2, 0.7, 0.2],   # green
         [0.7, 0.45, 0.2],  # orange
         [0.57, 0.2, 0.7],  # purple
-        [0.7, 0.7, 0.2],  # yellow
-        [0.2, 0.2, 0.7],  # dark blue
-        [0.7, 0.2, 0.2],  # red
-        [0.7, 0.7, 0.7],  # lavender
-        [0.2, 0.7, 0.7],  # sky blue
-        [0.2, 0.7, 0.5],  # teal
-        [0.5, 0.5, 0.5]  # gray
+        [0.7, 0.7, 0.2],   # yellow
+        [0.2, 0.2, 0.7],   # dark blue
+        [0.7, 0.2, 0.2],   # red
+        [0.7, 0.7, 0.7],   # lavender
+        [0.2, 0.7, 0.7],   # sky blue
+        [0.2, 0.7, 0.5],   # teal
+        [0.5, 0.5, 0.5]    # gray
     ]
     
     for s in range(len(matches)):
@@ -161,12 +172,12 @@ def draw_figures(matches):
             if sequence[i].isupper():
                 end = i
                 break
-        print(start)
-        print(end)
+
         # Draw exon
         # x,y of top left corner, width, height
-        ctx.rectangle(start*scale_factor, 750 + 1000 * s,
-                      (end-start)*scale_factor, 100)
+        # offset by 100 for margins
+        ctx.rectangle(start*scale_factor + 100, 775 + 1000 * s,
+                      (end-start)*scale_factor, 50)
         ctx.fill()
 
         # Draw motifs
@@ -176,7 +187,9 @@ def draw_figures(matches):
             ctx.set_source_rgb(colors[i][0], colors[i][1], colors[i][2])
             for match_position in range(len(motifs[i][1])):
                 start_position = motifs[i][1][match_position]
-                ctx.rectangle(start_position*scale_factor + 100, 775 + 1000 * s, 10, 50)
+                # offset by 100 for margins
+                ctx.rectangle(start_position*scale_factor + 100,
+                              775 + 1000 * s, 10, 50)
                 ctx.fill()
 
         # Sequence ID Title
@@ -197,6 +210,17 @@ def draw_figures(matches):
             ctx.fill()
             ctx.set_source_rgb(0, 0, 0)
 
+        # Scale Bar
+        ctx.move_to(100, 275 + 1000 * s)
+        ctx.set_font_size(36)
+        ctx.show_text("Scale")
+        ctx.set_font_size(24)
+        ctx.move_to(100, 325 + 1000 * s)
+        ctx.show_text(str(scale_bar_length) + "bp")
+        ctx.move_to(100, 350 + 1000 * s)
+        ctx.line_to(100 + scale_factor * scale_bar_length, 350 + 1000 * s)
+        ctx.stroke()
+
         # Divider to separate next figure
         ctx.move_to(0, 1000 + 1000 * s)
         ctx.set_source_rgb(0.5, 0.5, 0.5)
@@ -209,13 +233,7 @@ def main():
     sequence_list = parse_input_sequence(args.sequence_input)
     motif_list = parse_input_motifs(args.motifs_input)
     matches = find_motif_indices(motif_list, sequence_list)
-    # print(matches[0]) # First sequence and matches
-    # print(matches[0][0]) # First sequence ID
-    # print(matches[0][2]) # First sequence list of matching motifs
-    # print(matches[0][2][0]) # First sequence first motif, ID and indices
-    # print(matches[0][2][0][0])  # First sequence first motif ID
-    # print(matches[0][2][0][1])  # first sequence first motif list of indices
-    draw_figures(matches)
+    draw_figures(matches, args.output, args.scale_bar_length)
 
 
 if __name__ == "__main__":
